f6aea9bb04c9277d304364497abf4aa3f7442fc0
Charmi Bhikadiya <>
2024-09-03 18:31:09 -0700

    remove cms articles landing page

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,9 @@
 import HomePage from './components/HomePage';
 import SearchResults from './components/SearchResults';
-import DocumentationPage from './components/DocumentationPage'
 
 const routes = [
   { path: '/', element: <HomePage basename={window.__DOCUMENTATION_BASEROUTE__ || ''} /> },
-  { path: '/:query', element: <SearchResults /> },
-  { path: '/docs/*', element: <DocumentationPage basename={window.__DOCUMENTATION_BASEROUTE__ || ''} /> },
-  { path: '/docs', element: <DocumentationPage basename={window.__DOCUMENTATION_BASEROUTE__ || ''} /> }
+  { path: '/:query', element: <SearchResults /> }
 ];
 
 export default routes;
--- src/utils/logger.js
+++
@@ -1,5 +0,0 @@
-export default function logger(label) {
-    return {
-        debug: (message) => console.debug(`[${label}] ${message}`)
-    };
-}
