72f64fa50fcb0396101304733ff73378af469f93
Charmi Bhikadiya <>
2024-09-24 16:40:35 -0700

    clean code

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import HomePage from './components/HomePage';
 import SearchResults from './components/SearchResults';
 import VideoLandingPage from './components/VideoLandingPage';
 
+const basename = window.__DOCUMENTATION_BASEROUTE__ || '';
+
 const routes = [
-  { path: '/', element: <HomePage basename={window.__DOCUMENTATION_BASEROUTE__ || ''} /> },
-  { path: '/:query', element: <SearchResults basename={window.__DOCUMENTATION_BASEROUTE__ || ''}/> },
-  { path: '/videos', element: <VideoLandingPage basename={window.__DOCUMENTATION_BASEROUTE__ || ''}/> }, 
+  { path: '/', element: <HomePage basename={basename} /> },
+  { path: '/:query', element: <SearchResults basename={basename}/> },
+  { path: '/videos', element: <VideoLandingPage basename={basename}/> }, 
 ];
 
 export default routes;
