3b2925a6d9d7417393cf9320c3d885ff60fe5fe2
Charmi Bhikadiya <>
2026-05-18 14:02:52 -0700

    styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,15 +2,16 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import HelpTopicCards from './HelpTopicCards';
 import HelpMenu from './HelpMenu';
+import './HomePage.css';
 
 const HomePage = ({ basename }) => {
   return (
-    <div>
+    <div className="help-homepage">
       <div className="row">
         <div className="col-lg-9 col-md-9 col-sm-12">
-          <h1 className="text-left mb-3">RCSB PDB Help</h1>
+          <h1 className="help-homepage__title">RCSB PDB Help</h1>
         </div>
-        <div className="col-lg-3 col-md-3 col-sm-12">
+        <div className="col-lg-3 col-md-3 col-sm-12 help-homepage__top-links">
           <HelpMenu basename={basename} />
         </div>
       </div>
